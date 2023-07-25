db228e3393583b1c8aad1bb829177e53883bf66d
Nwalozie <iyekcnwalozie@gmail.com>
2023-07-25 14:34:20 -0700

    C - Sorting algorithms & Big O

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,23 +1,28 @@
 #include "sort.h"
 
+/**
+ * shell_sort - Function to sort an array of integers using Shell Sort algorithm
+ * @array: Pointer to the array to be sorted
+ * @size: Number of elements in the array
+ */
 void shell_sort(int *array, size_t size)
 {
-        size_t gap, i, j;
-        int temp;
+	size_t gap, i, j;
+	int temp;
 
-        for (gap = size / 2; gap > 0; gap /= 2)
-        {
-                for (i = gap; i < size; i++)
-                {
-                        temp = array[i];
+	for (gap = size / 2; gap > 0; gap /= 2)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
 
-                        for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-                        {
-                                array[j] = array[j - gap];
-                        }
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
 
-                        array[j] = temp;
-                }
-                print_array(array, size);
-        }
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,25 +10,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j;
-        size_t min_idx, temp;
+	size_t i, j;
+	size_t min_idx, temp;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                min_idx = i;
-                for (j = i + 1; j < size; j++)
-                {
-                        if (array[j] < array[min_idx])
-                                min_idx = j;
-                }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-                if (min_idx != i)
-                {
-                        temp = array[i];
-                        array[i] = array[min_idx];
-                        array[min_idx] = temp;
-                }
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+		}
 
-                print_array(array, size);
-        }
+		print_array(array, size);
+	}
 }
